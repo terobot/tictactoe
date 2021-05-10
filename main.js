c483a46c2fc2b5d7209d1b660bca3914ba448152
terobot <tero.t.kononen@gmail.com>
2021-05-10 23:30:39 +0300

    Add some spagetti code

--- main.js
+++ main.js
@@ -64,18 +64,19 @@ const GameBoard = (() => {
         gameBoard[row][col] = mark
         element.innerHTML = mark
     }
-    return { gameBoard, setupGameBoard, updateBoard }
+    return { gameBoard, setupGameBoard, updateBoard, calcCol, calcRow }
 })()
-const Player = (name, type, mark, score) => {
-    return {name, type, mark, score}
+const Player = (name, type, mark, score, turn) => {
+    return {name, type, mark, score, turn}
 }
 const GameManager = (() => {
     const gameParameters = {
         gameType: 1,
         gameBoardSize: 3,
         difficulty: 'Easy',
-        player1: Player('You', 'human', 'x', 0),
-        player2: Player('Bot', 'ai', 'o', 0)
+        player1: Player('You', 'human', 'x', 0, true),
+        player2: Player('Bot', 'ai', 'o', 0, false),
+        turn: 'x'
     }
     const drawQuestions = (gameType) => {
         const bodyHeader = document.getElementsByTagName('header')[0]
@@ -99,24 +100,46 @@ const GameManager = (() => {
         questionsEl.append(twoPlayerEl)
         bodyHeader.insertAdjacentElement('afterend',questionsEl)
     }
-    // const drawStats = () => {
-    //     const bodyHeader = document.getElementsByTagName('header')[0]
-    //     const statsEl = document.createElement('div')
-    //     const playerOneEl = document.createElement('div')
-    //     const playerTwoEl = document.createElement('div')
-
-    //     statsEl.append(playerOneEl)
-    //     statsEl.append(playerTwoEl)
-    //     bodyHeader.insertAdjacentElement('afterend',statsEl)
-    // }
+    const createStatsEl = () => {
+        const statsEl = document.createElement('div')
+        statsEl.setAttribute('class', 'stats')
+        return statsEl
+    }
+    const createPlayerStatsEl = (player) => {
+        const playerStatsEl = document.createElement('div')
+        const nameAndMarkEl = document.createElement('div')
+        const scoreEl = document.createElement('div')
+        playerStatsEl.setAttribute('class', 'stats-player')
+        nameAndMarkEl.innerHTML = `${player.name} play '${player.mark.toUpperCase()}'`
+        playerStatsEl.append(nameAndMarkEl)
+        scoreEl.innerHTML = player.score
+        playerStatsEl.append(scoreEl)
+        return playerStatsEl
+    }
+    const drawStats = () => {
+        const bodyHeader = document.getElementsByTagName('header')[0]
+        const statsEl = createStatsEl()
+        const playerOneStatsEl = createPlayerStatsEl(gameParameters.player1)
+        const playerTwoStatsEl = createPlayerStatsEl(gameParameters.player2)
+        statsEl.append(playerOneStatsEl)
+        statsEl.append(playerTwoStatsEl)
+        bodyHeader.insertAdjacentElement('afterend',statsEl)
+    }
     const initDisplay = () => {
         const questionsEl = document.getElementsByClassName('questions')[0]
+        const statsEl = document.getElementsByClassName('stats')[0]
         const gameBoardEl = document.getElementsByClassName('game-container')[0]
         if(questionsEl) {
             while (questionsEl.lastElementChild) {
                 questionsEl.lastElementChild.remove()
             }
             questionsEl.remove()
+        }
+        if(statsEl) {
+            while (statsEl.lastElementChild) {
+                statsEl.lastElementChild.remove()
+            }
+            statsEl.remove()
         }
         if(gameBoardEl) {
             while (gameBoardEl.lastElementChild) {
@@ -129,33 +152,74 @@ const GameManager = (() => {
         initDisplay()
         drawQuestions(gameParameters.gameType)
         GameBoard.setupGameBoard(gameParameters.gameBoardSize)
+        drawStats()
         console.log(gameParameters)
     }
     const initSinglePlayerGame = () => {
         gameParameters.gameType = 1
-        gameParameters.player1 = Player('You', 'human', 'x', 0)
-        gameParameters.player2 = Player('Bot', 'ai', 'o', 0)
+        gameParameters.player1 = Player('You', 'human', 'x', 0, true)
+        gameParameters.player2 = Player('Bot', 'ai', 'o', 0, false)
         newGame()
     }
     const initTwoPlayerGame = () => {
         gameParameters.gameType = 2
-        gameParameters.player1 = Player('Player1', 'human', 'x', 0)
-        gameParameters.player2 = Player('Player2', 'human', 'o', 0)
+        gameParameters.player1 = Player('Player1', 'human', 'x', 0, true)
+        gameParameters.player2 = Player('Player2', 'human', 'o', 0, false)
         newGame()
+    }
+    const getRandomIntInclusive = (min, max) => {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+    const playBotMove = (difficulty, botMark, board, size) => {
+        if (difficulty === 'Easy') {
+            var movePlayed = false
+            while (!movePlayed) {
+                var index = getRandomIntInclusive(1, size*size)
+                var element = document.getElementById(`${index}`)
+                if (element.innerHTML === '') {
+                    GameBoard.updateBoard(element, index, botMark)
+                    movePlayed = true
+                }
+            }
+        }
+    }
+    const controlGamePlay = (gameType) => {
+        if (gameType === 1) {
+            playBotMove(gameParameters.difficulty, gameParameters.player2.mark, GameBoard.gameBoard, gameParameters.gameBoardSize)
+        }
+        else {
+            changeTurn(gameParameters.turn)
+        }
+    }
+    const checkForEmptyCell = (board) => {
+        board.forEach(e => {
+            if (e === '') {
+                return true
+            }
+        })
+        return false
+    }
+    const checkWinningRow = (index, mark, size) => {
+        const row = GameBoard.calcRow(index)
+        const col = GameBoard.calcCol(index, row)
+        return false
+    }
+    const changeTurn = (turn) => {
+        gameParameters.turn = turn === 'x' ? 'o' : 'x'
     }
     const placeMark = (e) => {
         const element = e.target
         const index = Number(e.target.id)
         if(element.innerHTML==='') {
-            GameBoard.updateBoard(element, index, gameParameters.player1.mark)
+            GameBoard.updateBoard(element, index, gameParameters.turn)
         }
+        checkWinningRow(index, gameParameters.turn, gameParameters.size)
+        controlGamePlay(gameParameters.gameType)
         e.preventDefault()
     }
     return { newGame, placeMark }
 })()
 
 GameManager.newGame()
-// GameBoard.updateBoard(5,'o')
-// GameBoard.updateBoard(1,'x')
-// GameBoard.updateBoard(9,'o')
-// GameBoard.updateBoard(7,'x')
